d5709c4fb20e5787f1723d17b044ff898a704853
emanuelgamer011 <90418449+emanuelgamer011@users.noreply.github.com>
2022-05-15 07:43:55 -0500

    fix error in the login

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,3 @@
--- Modifying or rewriting this resource for local use only is strongly discouraged.
--- Feel free to open an issue or pull request in our GitHub.
--- Official discord server: https://discord.gg/AFAAXzq
-
 author 'ΞMΛNUΞL#5620'
 description 'Five Panel'
 version '1.0'
